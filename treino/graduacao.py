21db0fa9ff7f6e81cc2955472c7f9d1037731828
programadorpython03 <programadorpython03@gmail.com>
2025-02-17 10:44:51 -0300

    Correção da conta que verifica se o aluno pode trocar de faixa.

--- treino/graduacao.py
+++ treino/graduacao.py
@@ -12,6 +12,6 @@ order_belts ={ "Branca": 0, "Azul": 1, "Roxa": 2, "Marrom": 3, "Preta": 4 }
 def calculate_lesson_to_upgrade(n):
   d = 1.47
   k = 30 / math.log(d)
-  aulas = k * math.log(n)
+  aulas = k * math.log(n + d)
 
   return round(aulas)
